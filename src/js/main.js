fbf655a988f43d138b0b6c3f6e5cdcd863092071
rajwindersxxx <rajwindersxxx@gmail.com>
2024-10-31 16:34:17 +0530

    break

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 const menuButton = document.querySelector(".menu_button");
 const overlayModel = document.querySelector(".overlay_model");
-const navList = document.querySelector(".nav_list");
+const navLinks = document.querySelector('.nav_link-model')
+menuButton.addEventListener("click", () => {
+    overlayModel.classList.toggle("hidden");
+});
